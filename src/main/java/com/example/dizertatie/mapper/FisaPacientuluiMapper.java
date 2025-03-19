7434f5bdbd20035137fb51cdd64029bfb93bb370
David Denis <denisdbl331@gmail.com>
2025-03-19 21:57:18 +0200

    Fisa pacientului - mapper,repo

---
+++ src/main/java/com/example/dizertatie/mapper/FisaPacientuluiMapper.java
@@ -0,0 +1,49 @@
+package com.example.dizertatie.mapper;
+
+import com.example.dizertatie.dto.FisaPacientuluiDto;
+import com.example.dizertatie.entities.FisaPacientului;
+
+public class FisaPacientuluiMapper {
+
+    // Conversie DTO -> Entitate (fără medicamenteConcomitente, dataNasterii, sex)
+    public static FisaPacientului fisaPacientuluiDto2Entity(FisaPacientuluiDto fisaDto) {
+        FisaPacientului fisa = new FisaPacientului();
+
+        fisa.setGreutate(fisaDto.getGreutate());
+        fisa.setInaltime(fisaDto.getInaltime());
+        fisa.setAlergii(fisaDto.getAlergii());
+        fisa.setReactiiAdverse(fisaDto.getReactiiAdverse());
+        fisa.setMedicamenteSuspecte(fisaDto.getMedicamenteSuspecte());
+        fisa.setEvolutieReactie(fisaDto.getEvolutieReactie());
+        fisa.setNumeRaportor(fisaDto.getNumeRaportor());
+        fisa.setRelatieCuPacientul(fisaDto.getRelatieCuPacientul());
+
+        // Pacientul se setează separat în serviciu (prin ID)
+        return fisa;
+    }
+
+    // Conversie Entitate -> DTO
+    public static FisaPacientuluiDto fisaPacientuluiEntity2Dto(FisaPacientului fisa) {
+        if (fisa.getId() == null) {
+            return null;
+        }
+
+        FisaPacientuluiDto fisaDto = new FisaPacientuluiDto();
+
+        fisaDto.setGreutate(fisa.getGreutate());
+        fisaDto.setInaltime(fisa.getInaltime());
+        fisaDto.setAlergii(fisa.getAlergii());
+        fisaDto.setReactiiAdverse(fisa.getReactiiAdverse());
+        fisaDto.setMedicamenteSuspecte(fisa.getMedicamenteSuspecte());
+        fisaDto.setEvolutieReactie(fisa.getEvolutieReactie());
+        fisaDto.setNumeRaportor(fisa.getNumeRaportor());
+        fisaDto.setRelatieCuPacientul(fisa.getRelatieCuPacientul());
+
+        // Legătura cu pacientul (prin ID)
+        if (fisa.getPacient() != null) {
+            fisaDto.setPacientId(fisa.getPacient().getId());
+        }
+
+        return fisaDto;
+    }
+}
---
+++ src/main/java/com/example/dizertatie/repository/FisaPacientuluiRepository.java
@@ -0,0 +1,8 @@
+package com.example.dizertatie.repository;
+
+import com.example.dizertatie.entities.FisaPacientului;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FisaPacientuluiRepository extends JpaRepository<FisaPacientului, Long> {
+
+}
